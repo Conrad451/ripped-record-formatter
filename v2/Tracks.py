939509e46bd202df26e1ffe16a3e236d5709b99d
Conrad451 <67305888+Conrad451@users.noreply.github.com>
2024-11-19 17:04:33 -0600

    import_tracks.py
    Functions for importing tracks and adding values

--- v2/Tracks.py
+++ v2/Tracks.py
@@ -1,18 +1,22 @@
 class Tracks:
-    def __init__(self, track_num, track_name, track_album, track_artist):
+    def __init__(self, track_num, track_name, track_album, track_artist, track_wav_loc):
         self.track_num = track_num
         self.track_name = track_name
         self.track_album = track_album
         self.track_artist = track_artist
+        self.track_wav_loc = track_wav_loc
 
     def __str__(self):
         if int(self.track_num) < 10:
-            return f"[0{self.track_num}] - {self.track_name}"
+            return f"[0{self.track_num}] - {self.track_name}.flac"
         else:
-            return f"[{self.track_num}] - {self.track_name}"
+            return f"[{self.track_num}] - {self.track_name}.flac"
 
     def track_album(self):
         return self.track_album
 
     def track_artist(self):
         return self.track_artist
+
+    def track_wav_name(self):
+        return self.track_wav_loc
---
+++ v2/import_tracks.py
@@ -0,0 +1,49 @@
+from Tracks import Tracks
+import tkinter as tk
+from tkinter import filedialog
+import os
+
+
+
+def wavdir_flacdir():
+    root = tk.Tk()
+    root.withdraw()
+
+    while True:
+
+        print("Select Album Directory")
+        wavdir = filedialog.askdirectory()
+        print(f"Sourcing files from {wavdir}")
+
+        print("Select Write Directory")
+        flacdir = filedialog.askdirectory()
+        print(f"Saving files to {flacdir}")
+
+        continue_input = input("Continue with operations? Y/N:  ")
+
+        if continue_input == "Y" or continue_input == "y":
+            return wavdir, flacdir
+        else:
+            pass
+
+def wavdir_tracklist(wavdir, track_album, track_artist):
+    track_data = []
+    wavTracklist = os.listdir(wavdir)
+    track_num = 1
+    for track in wavTracklist:
+        track_name = input(f"Enter the track name for {track}: ")
+        track_wav_loc = os.path.join(wavdir, track)
+        track = Tracks(track_num, track_name, track_album, track_artist, track_wav_loc )
+        track_data.append(track)
+        track_num += 1
+    return track_data
+
+def get_loc_save_tracks():
+    wavdir, flacdir = wavdir_flacdir()
+    track_album = input("Enter the album name: ")
+    track_artist = input("Enter the artist name: ")
+    track_data = wavdir_tracklist(wavdir,track_album,track_artist)
+
+    return track_data
+
+
