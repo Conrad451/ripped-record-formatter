fd7d4f9a6b9844b87f0c6c7928ebc74c5e3bd6a0
Conrad451 <67305888+Conrad451@users.noreply.github.com>
2025-03-02 17:25:37 -0600

    added progress bar

--- v2/wav_to_flac.py
+++ v2/wav_to_flac.py
@@ -5,14 +5,17 @@
 
 from pydub import AudioSegment
 import os
+from alive_progress import alive_bar
 
 def wav_to_flac(track_data, flacdir):
     track_num = 1
-    for track in track_data:
-        flac = AudioSegment.from_wav(track.track_wav_loc)
-        flac.export(flacdir + "\\" + track.__str__(), format="flac", tags = {'artist': track.track_artist, 'album': track.track_album,
-                                                                             'title': track.track_name , 'track': track_num})
-        track_num += 1
+    with alive_bar(len(track_data)) as bar:
+        for track in track_data:
+            bar()
+            flac = AudioSegment.from_wav(track.track_wav_loc)
+            flac.export(flacdir + "\\" + track.__str__(), format="flac", tags = {'artist': track.track_artist, 'album': track.track_album,
+                                                                                 'title': track.track_name , 'track': track_num})
+            track_num += 1
 
     return "Operations Complete"
 
